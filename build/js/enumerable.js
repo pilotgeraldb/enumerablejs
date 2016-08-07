085bd9d91339e105a4f8e5ea58273f502c0f5707
Gerald Burkholder <geraldeburkholder@gmail.com>
2016-08-07 18:52:30 -0500

    added take while and tests. take while tests not passing.

--- build/js/enumerable.js
+++ build/js/enumerable.js
@@ -284,6 +284,29 @@ var Enumerable = (function ()
         return false;
     };
 
+    _enumerable.prototype.takeWhile = function (fn)
+    {
+        var results = [];
+
+        for (var i = 0; i < this.collection.length; i++)
+        {
+            var item = this.collection[i];
+
+            var _callbackResult = fn(i, item, this.collection);
+
+            if (_callbackResult !== true)
+            {
+                return results.asEnumerable();;
+            }
+            else
+            {
+                results.push(this.collection[i]);
+            }
+        }
+
+        return results.asEnumerable();
+    };
+
     _enumerable.prototype.skip = function (count)
     {
         var results = [];
---
+++ spec/takeWhile.js
@@ -0,0 +1,44 @@
+﻿describe("take while -- multiple items in collection", function ()
+{
+    it("description here", function ()
+    {
+        var testArray = [{ name: 'test', id: 1 }, { name: '1234', id: 2 }, { name: '5678', id: 3 }];
+
+        var result = testArray.asEnumerable().takeWhile(function (i, item, collection)
+        {
+            return (item.name === "test");
+        });
+
+        expect(result).toEqual([{ name: '1234', id: 2 }, { name: '5678', id: 3 }].asEnumerable());
+    });
+});
+
+describe("take while -- single item collection", function ()
+{
+    it("description here", function ()
+    {
+        var testArray = [{ name: 'test', id: 1 }];
+
+        var result = testArray.asEnumerable().takeWhile(function (i, item, collection)
+        {
+            return (item.name === "test");
+        });
+
+        expect(result).toEqual([].asEnumerable());
+    });
+});
+
+describe("take while  -- empty collection", function ()
+{
+    it("description here", function ()
+    {
+        var testArray = [];
+
+        var result = testArray.asEnumerable().takeWhile(function (i, item, collection)
+        {
+            return (item.name === "test");
+        });
+
+        expect(result).toEqual(testArray.asEnumerable());
+    });
+});
