b140760adea249e7f41ffbdffa9bae09b2a5523c
ralphy15 <geraldeburkholder@gmail.com>
2017-12-01 10:53:34 -0600

    fixed skipWhile to return correct results

--- spec/skipWhile.js
+++ spec/skipWhile.js
@@ -2,24 +2,39 @@
 
 describe("skip while", function ()
 {
-    describe("when an enumerable object has 3 items", function ()
+    describe("when an enumerable object has multiple items", function ()
     {
-        it("should skip items that match a predicate function and return the others", function ()
+        it("should return everything after the item where the predicate function evaluated to false", function ()
         {
-            var testArray = new Enumerable([{ name: 'test', id: 1 }, { name: '1234', id: 2 }, { name: '5678', id: 3 }]);
+            var testArray = new Enumerable([{ name: 'test', id: 1 }, { name: '1234', id: 2 }, { name: '5678', id: 3 }, { name: '9012', id: 4 }]);
 
             var result = testArray.skipWhile(function (i, item, collection)
             {
-                return (item.name === "test");
+                return (item.id < 3);
             });
 
-            expect(result).toEqual(new Enumerable([{ name: '1234', id: 2 }, { name: '5678', id: 3 }]));
+            expect(result).toEqual(new Enumerable([{ name: '5678', id: 3 }, { name: '9012', id: 4 }]));
+        });
+    });
+
+    describe("when an enumerable object has multiple numbers", function ()
+    {
+        it("should return everything after the item where the predicate function evaluated to false", function ()
+        {
+            var testArray = new Enumerable([98, 92, 85, 82, 70, 59, 56]);
+
+            var result = testArray.skipWhile(function (i, item, collection)
+            {
+                return (item >= 80);
+            });
+
+            expect(result).toEqual(new Enumerable([70, 59, 56]));
         });
     });
 
     describe("when an enumerable object has 1 item", function ()
     {
-        it("should skips items that match a predicate function and return the others", function ()
+        it("should return everything after the item where the predicate function evaluated to false", function ()
         {
             var testArray = new Enumerable([{ name: 'test', id: 1 }]);
 
@@ -46,4 +61,23 @@ describe("skip while", function ()
             expect(result).toEqual(new Enumerable([]));
         });
     });
+
+    describe("when an enumerable object has multiple items and the predicate return true midway through the collection", function ()
+    {
+        it("should return everything after the item where the predicate function evaluated to false", function ()
+        {
+            var testArray = new Enumerable([{ name: 'test', id: 1 }, { name: '1234', id: 2 }, { name: '5678', id: 3 }]);
+
+            var result = testArray.skipWhile(function (i, item, collection)
+            {
+                return (item.id < 3);
+            });
+
+            expect(result).toEqual(new Enumerable([{ name: '5678', id: 3 }]));
+        });
+    });
+
+    //skip while >= 80
+    //98, 92, 85, 82, 70, 59, 56
+    //returns 70, 59, 56
 });
--- src/core/skipWhile.js
+++ src/core/skipWhile.js
@@ -6,17 +6,29 @@ Enumerable.fn.skipWhile = function(fn)
     var hasFn = (fn !== null && fn !== undefined);
     var FnIsFunction = (hasFn && typeof fn === "function");
     var canSkip = (hasFn && FnIsFunction);
+    var hasItems = this.collection.length > 0;
 
-    if(canSkip)
+    if(!hasFn || !FnIsFunction)
     {
+        return this;
+    }
+
+    if(canSkip && hasItems)
+    {
+        var end = this.collection.length;
+        
         for(var i = 0; i < this.collection.length; i++)
         {
             var item = this.collection[i];
             var _callbackResult = fn(i, item, this.collection);
 
-            if(_callbackResult !== true)
+            if(_callbackResult === true)
             {
-                results.push(this.collection[i]);
+                continue;
+            }
+            else
+            {
+                return this.collection.slice(i, end).asEnumerable();
             }
         }
     }
