8841fbc470bf60f7adee32a27ea690c3196a0932
ralphy15 <geraldeburkholder@gmail.com>
2018-03-27 20:05:17 -0500

    updated select to use projection function

--- src/core/select.js
+++ src/core/select.js
@@ -4,28 +4,45 @@ Enumerable.fn.select = function(obj)
 {
     var results = [];
 
+    function explicit(item, obj)
+    {
+        var _temp = {};
+            
+        for(var x = 0; x < obj.length; x++)
+        {
+            var prop = obj[x];
+
+            for(var p in item)
+            {
+                if(p === prop)
+                {
+                    _temp[prop] = item[p];
+                }
+            }
+        }
+        
+        results.push(_temp);
+    }
+
+    function funcEval(obj, i, item, col)
+    {
+        results.push(obj(i, item, col));
+    }
+
     for(var i = 0; i < this.collection.length; i++)
     {
         var item = this.collection[i];
 
         if(obj !== null && obj !== undefined && obj.length > 0)
         {
-            var _temp = {};
-            
-            for(var x = 0; x < obj.length; x++)
+            if(obj.getType() == 'function')
             {
-                var prop = obj[x];
-
-                for(var p in item)
-                {
-                    if(p === prop)
-                    {
-                        _temp[prop] = item[p];
-                    }
-                }
+                funcEval(obj, i, item, this.collection);
             }
-            
-            results.push(_temp);
+            else
+            {
+                explicit(item, obj);
+            }
         }
         else
         {
