ecb1b60630cc759ec3a2dd8e78d2a3e72d55133f
Gerald Burkholder <ralphy15@users.noreply.github.com>
2018-03-27 20:39:41 -0500

    added Enumerable.Range (#18)
    * updated select to use projection function
    
    * added Enumerable.Range
    
    * added negative numbers test for Enumerable.Range
    
    * updated select method and added tests
    
    * updated select documentation

--- spec/select.js
+++ spec/select.js
@@ -4,25 +4,37 @@ describe("select", function ()
 {
     describe("when an enumerable object has 3 items", function ()
     {
-        it("should return 3 objects containing only the selected fields", function ()
+        it("it should return 3 value types", function ()
         {
             var testArray = new Enumerable([{ name: 'test', id: 1 }, { name: '1234', id: 2 }, { name: '5678', id: 3 }]);
 
             var result = testArray.select(["name"]);
 
-            expect(result).toEqual(new Enumerable([{ name: "test" }, { name: "1234" }, { name: "5678" }]));
+            expect(result).toEqual(new Enumerable(["test", "1234", "5678"]));
         });
     });
 
-    describe("when an enumerable object contains only 1 item", function ()
+    describe("when an enumerable object has 3 items and multiple properties are selected via string array", function ()
     {
-        it("should return 1 object containing only the selected fields", function ()
+        it("it should return 3 objects containing only the selected fields", function ()
+        {
+            var testArray = new Enumerable([{ name: 'test', id: 1, x:1 }, { name: '1234', id: 2, x:1 }, { name: '5678', id: 3, x:1 }]);
+
+            var result = testArray.select(["name", "id"]);
+
+            expect(result).toEqual(new Enumerable([{ name: 'test', id: 1 }, { name: '1234', id: 2 }, { name: '5678', id: 3 }]));
+        });
+    });
+
+    describe("when an enumerable object contains only 1 item and a single property is selected via string array", function ()
+    {
+        it("it should return 1 value type", function ()
         {
             var testArray = new Enumerable([{ name: 'test', id: 1 }]);
 
             var result = testArray.select(["name"]);
 
-            expect(result).toEqual(new Enumerable([{ name: "test" }]));
+            expect(result).toEqual(new Enumerable(["test"]));
         });
     });
 
@@ -33,6 +45,36 @@ describe("select", function ()
             var testArray = new Enumerable([]);
 
             var result = testArray.select(["name"]);
+
+            expect(result).toEqual(new Enumerable([]));
+        });
+    });
+
+    describe("when using a projection function", function ()
+    {
+        it("it should return projection", function ()
+        {
+            var testArray = new Enumerable([{ name: 'test', id: 1 }, { name: '1234', id: 2 }, { name: '5678', id: 3 }]);
+
+            var result = testArray.select(function(i, item, col)
+                        {
+                            return { name: item.name };
+                        });
+
+            expect(result).toEqual(new Enumerable([{ name: "test" }, { name: "1234" }, { name: "5678" }]));
+        });
+    });
+
+    describe("when an enumerable object is empty and using a projection function", function ()
+    {
+        it("it should return an empty enumerable object", function ()
+        {
+            var testArray = new Enumerable([]);
+
+            var result = testArray.select(function(i, item, col)
+                        {
+                            return { name: item.name };
+                        });
 
             expect(result).toEqual(new Enumerable([]));
         });
--- src/core/select.js
+++ src/core/select.js
@@ -4,14 +4,23 @@ Enumerable.fn.select = function(obj)
 {
     var results = [];
 
-    for(var i = 0; i < this.collection.length; i++)
+    function explicit(item, obj)
     {
-        var item = this.collection[i];
-
-        if(obj !== null && obj !== undefined && obj.length > 0)
+        var _temp = {};
+        
+        if(obj.length == 1)
         {
-            var _temp = {};
-            
+            for(var p in item)
+            {
+                if(p === obj[0])
+                {
+                    _temp = item[p];
+                    break;
+                }
+            }
+        }
+        else
+        {
             for(var x = 0; x < obj.length; x++)
             {
                 var prop = obj[x];
@@ -24,8 +33,30 @@ Enumerable.fn.select = function(obj)
                     }
                 }
             }
-            
-            results.push(_temp);
+        }
+        
+        results.push(_temp);
+    }
+
+    function funcEval(obj, i, item, col)
+    {
+        results.push(obj(i, item, col));
+    }
+
+    for(var i = 0; i < this.collection.length; i++)
+    {
+        var item = this.collection[i];
+
+        if(obj !== null && obj !== undefined && obj.length > 0)
+        {
+            if(obj.getType() == 'function')
+            {
+                funcEval(obj, i, item, this.collection);
+            }
+            else
+            {
+                explicit(item, obj);
+            }
         }
         else
         {
