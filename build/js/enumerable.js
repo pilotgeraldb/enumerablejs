02f87ad1b8370584a26d95cb3261b03b53f6a238
ralphy15 <geraldeburkholder@gmail.com>
2018-04-06 22:35:07 -0500

    bumped version built code

--- build/js/enumerable.js
+++ build/js/enumerable.js
@@ -77,13 +77,6 @@ return /******/ (function(modules) { // webpackBootstrap
 /* 0 */
 /***/ (function(module, exports, __webpack_require__) {
 
-Array.prototype.asEnumerable = function()
-{
-    var e = new Enumerable(this);
-
-    return e;
-};
-
 function Enumerable(_array)
 {
     if(_array)
@@ -97,6 +90,18 @@ function Enumerable(_array)
 }
 
 Enumerable.fn = Enumerable.prototype;
+
+Enumerable.Range = function(start, count)
+{
+    var result = [];
+
+    for(var i = 0; i < count; i++)
+    {
+        result[i]  = start + i;
+    }
+
+    return result.asEnumerable();
+};
 
 module.exports = Enumerable;
 
@@ -128,6 +133,10 @@ __webpack_require__(19);
 __webpack_require__(20);
 __webpack_require__(21);
 __webpack_require__(22);
+__webpack_require__(23);
+__webpack_require__(24);
+__webpack_require__(25);
+__webpack_require__(26);
 
 /***/ }),
 /* 2 */
@@ -173,7 +182,11 @@ Object.prototype.getType = function()
     else 	if(typeString === '[object Object]')
     {
       return 'object';
-    }    
+    } 
+    else if (typeof this === "function")
+    {
+      return 'function';
+    }   
     
     return typeString;
 };
@@ -197,7 +210,16 @@ var Enumerable = __webpack_require__(0);
 
 Enumerable.fn.any = function(fn)
 {
-    var hasFn = (fn !== null && fn !== undefined && typeof fn === 'function');
+    var hasFn = (fn !== null && fn !== undefined);
+    var FnIsFunction = (hasFn && typeof fn === "function");
+
+    if(!hasFn)
+    {
+        if(this !== null && this !== undefined && this.collection.length > 0)
+        {
+            return true;
+        }
+    }
 
     for(var i = 0; i < this.collection.length; i++)
     {
@@ -208,17 +230,11 @@ Enumerable.fn.any = function(fn)
             continue;
         }
 
-        if(hasFn && fn(i, item, this.collection))
+        if(hasFn && FnIsFunction && fn(i, item, this.collection))
         {
             return true;
         }
-        else if(hasFn)
-        {
-            if(this !== null && this !== undefined && this.collection.length > 0)
-            {
-                return true;
-            }
-        }
+        
     }
 
     return false;
@@ -309,6 +325,52 @@ Enumerable.fn.firstOrDefault = function(fn)
 
 var Enumerable = __webpack_require__(0);
 
+Enumerable.fn.dropFirst = function(count)
+{
+    if(count <= 0)
+    {
+        return new Enumerable(this.collection);
+    }
+
+    if(this.collection.length > count)
+    {
+        return new Enumerable(this.collection.splice(this.collection.length - count, count));
+    }
+    else
+    {
+        return new Enumerable();
+    }
+};
+
+/***/ }),
+/* 10 */
+/***/ (function(module, exports, __webpack_require__) {
+
+var Enumerable = __webpack_require__(0);
+
+Enumerable.fn.dropLast = function(count)
+{
+    if(count <= 0)
+    {
+        return new Enumerable(this.collection);
+    }
+
+    if(this.collection.length > count)
+    {
+        return new Enumerable(this.collection.splice(0, this.collection.length - count));
+    }
+    else
+    {
+        return new Enumerable();
+    }
+};
+
+/***/ }),
+/* 11 */
+/***/ (function(module, exports, __webpack_require__) {
+
+var Enumerable = __webpack_require__(0);
+
 Enumerable.fn.last = function()
 {
     if(this.collection.length > 0)
@@ -322,7 +384,7 @@ Enumerable.fn.last = function()
 };
 
 /***/ }),
-/* 10 */
+/* 12 */
 /***/ (function(module, exports, __webpack_require__) {
 
 var Enumerable = __webpack_require__(0);
@@ -353,7 +415,85 @@ Enumerable.fn.lastOrDefault = function(fn)
 };
 
 /***/ }),
-/* 11 */
+/* 13 */
+/***/ (function(module, exports, __webpack_require__) {
+
+var Enumerable = __webpack_require__(0);
+
+Enumerable.fn.shuffle = function()
+{
+    var currentIndex = this.collection.length;
+    var temporaryValue;
+    var randomIndex;
+
+    while (0 !== currentIndex) 
+    {
+      randomIndex = Math.floor(Math.random() * currentIndex);
+      currentIndex -= 1;
+    
+      temporaryValue = this.collection[currentIndex];
+      this.collection[currentIndex] = this.collection[randomIndex];
+      this.collection[randomIndex] = temporaryValue;
+    }
+    
+    return new Enumerable(this.collection);
+};
+
+/***/ }),
+/* 14 */
+/***/ (function(module, exports, __webpack_require__) {
+
+var Enumerable = __webpack_require__(0);
+
+Enumerable.fn.except = function(arr, fn)
+{
+    if(this.collection.length <= 0)
+    {
+        return new Enumerable(this.collection);
+    }
+
+    var result = [];
+
+    for(var i = 0; i < this.collection.length; i++)
+    {
+        var item = this.collection[i];
+        
+        var contains = false;
+
+        for(var x = 0; x < arr.length; x++)
+        {
+            var item2 = arr[x];
+
+            if(fn && fn.getType() === "function")
+            {
+                if(fn(item, item2) === true)
+                {
+                    contains = true;
+                    break;
+                }
+            }
+            else
+            {
+                if(item2 === item)
+                {
+                    contains = true;
+                    break;
+                }
+            }
+        }
+
+        if(contains === false)
+        {
+            result.push(item);
+        }
+    }
+
+    return result.asEnumerable();
+};
+
+
+/***/ }),
+/* 15 */
 /***/ (function(module, exports, __webpack_require__) {
 
 var Enumerable = __webpack_require__(0);
@@ -364,7 +504,7 @@ Enumerable.fn.maximum = function()
 
     function isNumeric(x)
     {
-        return (x.getType() === 'number') && (x % 1 === 0);
+        return (x !== null && x.getType() === 'number') && (x % 1 === 0);
     }
 
     var temp = [];
@@ -386,7 +526,7 @@ Enumerable.fn.maximum = function()
 };
 
 /***/ }),
-/* 12 */
+/* 16 */
 /***/ (function(module, exports, __webpack_require__) {
 
 var Enumerable = __webpack_require__(0);
@@ -397,7 +537,7 @@ Enumerable.fn.minimum = function()
 
     function isNumeric(x)
     {
-        return (x.getType() === 'number') && (x % 1 === 0);
+        return (x !== null && x.getType() === 'number') && (x % 1 === 0);
     }
 
     var temp = [];
@@ -414,12 +554,12 @@ Enumerable.fn.minimum = function()
     {
         result = Math.min.apply(null, temp);
     }
-    
+
     return result;
 };
 
 /***/ }),
-/* 13 */
+/* 17 */
 /***/ (function(module, exports, __webpack_require__) {
 
 var Enumerable = __webpack_require__(0);
@@ -546,7 +686,7 @@ Enumerable.fn.orderBy = function(property, fn)
 };
 
 /***/ }),
-/* 14 */
+/* 18 */
 /***/ (function(module, exports, __webpack_require__) {
 
 var Enumerable = __webpack_require__(0);
@@ -673,7 +813,7 @@ Enumerable.fn.orderByDescending = function(property, fn)
 };
 
 /***/ }),
-/* 15 */
+/* 19 */
 /***/ (function(module, exports, __webpack_require__) {
 
 var Enumerable = __webpack_require__(0);
@@ -682,14 +822,23 @@ Enumerable.fn.select = function(obj)
 {
     var results = [];
 
-    for(var i = 0; i < this.collection.length; i++)
+    function explicit(item, obj)
     {
-        var item = this.collection[i];
-
-        if(obj !== null && obj !== undefined && obj.length > 0)
+        var _temp = {};
+        
+        if(obj.length == 1)
         {
-            var _temp = {};
-            
+            for(var p in item)
+            {
+                if(p === obj[0])
+                {
+                    _temp = item[p];
+                    break;
+                }
+            }
+        }
+        else
+        {
             for(var x = 0; x < obj.length; x++)
             {
                 var prop = obj[x];
@@ -702,8 +851,30 @@ Enumerable.fn.select = function(obj)
                     }
                 }
             }
-            
-            results.push(_temp);
+        }
+        
+        results.push(_temp);
+    }
+
+    function funcEval(obj, i, item, col)
+    {
+        results.push(obj(i, item, col));
+    }
+
+    for(var i = 0; i < this.collection.length; i++)
+    {
+        var item = this.collection[i];
+
+        if(obj !== null && obj !== undefined && obj.length > 0)
+        {
+            if(obj.getType() == 'function')
+            {
+                funcEval(obj, i, item, this.collection);
+            }
+            else
+            {
+                explicit(item, obj);
+            }
         }
         else
         {
@@ -715,7 +886,7 @@ Enumerable.fn.select = function(obj)
 };
 
 /***/ }),
-/* 16 */
+/* 20 */
 /***/ (function(module, exports, __webpack_require__) {
 
 var Enumerable = __webpack_require__(0);
@@ -738,7 +909,7 @@ Enumerable.fn.single = function()
 };
 
 /***/ }),
-/* 17 */
+/* 21 */
 /***/ (function(module, exports, __webpack_require__) {
 
 var Enumerable = __webpack_require__(0);
@@ -774,7 +945,7 @@ Enumerable.fn.singleOrDefault = function(fn)
 
 
 /***/ }),
-/* 18 */
+/* 22 */
 /***/ (function(module, exports, __webpack_require__) {
 
 var Enumerable = __webpack_require__(0);
@@ -783,21 +954,33 @@ Enumerable.fn.skip = function(count)
 {
     var results = [];
 
-    var canSkip = ((count > 0) && (count < this.collection.length));
+    var hasItems = (count > 0);
+    var validSkipCount = (count < this.collection.length);
+
+    var canSkip = (hasItems && validSkipCount);
 
     if(canSkip)
     {
-        for(var i = (count - 1); i < count; i++)
+        for(var i = 0; i < this.collection.length; i++)
         {
+            if(i < count)
+            {
+                continue;
+            }
+            
             results.push(this.collection[i]);
         }
+    }
+    else
+    {
+        return this;
     }
 
     return results.asEnumerable();
 };
 
 /***/ }),
-/* 19 */
+/* 23 */
 /***/ (function(module, exports, __webpack_require__) {
 
 var Enumerable = __webpack_require__(0);
@@ -808,17 +991,29 @@ Enumerable.fn.skipWhile = function(fn)
     var hasFn = (fn !== null && fn !== undefined);
     var FnIsFunction = (hasFn && typeof fn === "function");
     var canSkip = (hasFn && FnIsFunction);
+    var hasItems = this.collection.length > 0;
 
-    if(canSkip)
+    if(!hasFn || !FnIsFunction)
     {
+        return this;
+    }
+
+    if(canSkip && hasItems)
+    {
+        var end = this.collection.length;
+        
         for(var i = 0; i < this.collection.length; i++)
         {
             var item = this.collection[i];
             var _callbackResult = fn(i, item, this.collection);
 
-            if(_callbackResult !== true)
+            if(_callbackResult === true)
             {
-                results.push(this.collection[i]);
+                continue;
+            }
+            else
+            {
+                return this.collection.slice(i, end).asEnumerable();
             }
         }
     }
@@ -827,7 +1022,7 @@ Enumerable.fn.skipWhile = function(fn)
 };
 
 /***/ }),
-/* 20 */
+/* 24 */
 /***/ (function(module, exports, __webpack_require__) {
 
 var Enumerable = __webpack_require__(0);
@@ -857,7 +1052,7 @@ Enumerable.fn.take = function(count)
 };
 
 /***/ }),
-/* 21 */
+/* 25 */
 /***/ (function(module, exports, __webpack_require__) {
 
 var Enumerable = __webpack_require__(0);
@@ -866,19 +1061,26 @@ Enumerable.fn.takeWhile = function(fn)
 {
     var results = [];
 
+    var hasFn = (fn !== null && fn !== undefined && typeof fn === 'function');
+
+    if(!hasFn)
+    {
+        return this;
+    }
+
     for(var i = 0; i < this.collection.length; i++)
     {
         var item = this.collection[i];
 
         var _callbackResult = fn(i, item, this.collection);
 
-        if(_callbackResult !== true)
+        if(_callbackResult === true)
         {
-            return results.asEnumerable();
+            results.push(item);
         }
         else
         {
-            results.push(this.collection[i]);
+            break;
         }
     }
 
@@ -886,7 +1088,7 @@ Enumerable.fn.takeWhile = function(fn)
 };
 
 /***/ }),
-/* 22 */
+/* 26 */
 /***/ (function(module, exports, __webpack_require__) {
 
 var Enumerable = __webpack_require__(0);
