418fcf1f5ef85f053d252bb361c04f281cc08dea
ralphy15 <geraldeburkholder@gmail.com>
2021-11-19 23:28:41 -0600

    update a unit test

--- spec/shuffle.js
+++ spec/shuffle.js
@@ -6,11 +6,11 @@ describe("shuffle", function ()
     {
         it("it should shuffle the items", function ()
         {
-            let testArray = new Enumerable([0, 1, 2, 3]);
+            let testArray = new Enumerable([0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]);
 
             let result = testArray.shuffle();
 
-            expect(result).not.toEqual(new Enumerable([0, 1, 2, 3]));
+            expect(result).not.toEqual(new Enumerable([0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]));
         });
     });
 });
